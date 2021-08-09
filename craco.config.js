66bffcb1c664650bd3b1e36b55c224740f6101ce
Subrahmanyam Padala <subrahmanyam.03@gmail.com>
2021-08-09 12:40:45 +0530

    build: add tsconfig path alias and eslint config

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,16 @@
+const CracoAlias = require('craco-alias');
+// const { CracoAliasPlugin, configPaths } = require('react-app-rewire-alias');
+
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
