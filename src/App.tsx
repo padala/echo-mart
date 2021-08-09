627bf3dfc521bbd448f4d89a5dac10b8afd32318
Subrahmanyam Padala <subrahmanyam.03@gmail.com>
2021-08-09 12:41:55 +0530

    build: add and configure react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Routes from './routes';
 
 import './App.css';
-import Header from './components/partials/header';
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <>
-      <Header />
+    <QueryClientProvider client={queryClient}>
       <Router>
         <Routes />
       </Router>
-    </>
+    </QueryClientProvider>
   );
 }
 
